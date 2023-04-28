711b50d92b3312a412fb8b833be3e0d274910fbe
dekim0705 <dekim0705@gmail.com>
2023-04-28 18:57:10 +0900

    :heavy_plus_sign: @mui/joy, @emotion/react,styled 의존성 추
    yarn install 해주세욥!

--- src/pages/JoinPage.js
+++ src/pages/JoinPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Logo from "../components/Logo"
+import Join from "../components/Account/JoinStep2";
 
 const JoinPage = () => {
 
   return (
     <>
+      <Logo />
+      <Join />
     </>
   );
 }
