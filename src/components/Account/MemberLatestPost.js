444b55e2de9600cd578958f02e397ed31b2ad536
dekim0705 <dekim0705@gmail.com>
2023-05-06 03:30:10 +0900

    :sparkles: 마이페이지 메인화면(?) 완료

---
+++ src/components/Account/MemberLatestPost.js
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react';
+import AccountAxiosApi from '../../api/AccountAxiosApi';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const ParentContainer = styled.div`
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  box-shadow: 1px 1px 3px 1px #C6DEF7;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+const Title = styled.h3`
+  align-self: flex-start;
+  margin-bottom: 10px;
+`;
+
+const StyledLink = styled.a`
+  position: absolute;
+  font-size: 0.9rem;
+  color: #1E2B4D;
+  align-self: flex-end;
+  text-decoration: underline;
+  margin-right: 10px;
+  &:hover {
+    font-weight: bold;
+  }
+`;
+
+const MyPostContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  & > *:not(:first-child) {
+    border-top: 1px solid #E5E7EA;
+  }
+  & > *:last-child {
+    border-bottom: 1px solid #E5E7EA;
+  }
+`;
+
+  const PostTitle = styled.p`
+  font-weight: bold;
+  font-size: 0.9rem;
+  margin-top: 10px;
+  width: 60%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis; 
+  &:hover {
+    text-decoration: underline;
+  }
+
+
+  `;
+
+  const ExtraInfo = styled.p`
+    text-align: end;
+    font-size: 0.8rem;
+  `;
+  
+const NoResult = styled.p`
+  padding: 20%;
+  font-size: 1.2rem;
+`;
+
+const PostContent = styled.p`
+font-size: 0.9rem;
+`;
+
+const MemberLatestPost= ({ userMemberNum }) => {
+  const [memberLatestPost, setMemberLatestPost] = useState([]);
+
+  useEffect(() => {
+    const fetchMemberLatestPost = async () => {
+      try {
+        const response = await AccountAxiosApi.getMemberLatestPost(userMemberNum);
+        setMemberLatestPost(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMemberLatestPost();
+  }, [userMemberNum]);
+
+  
+  return (
+      <ParentContainer>
+      <Title>내가 작성한 글</Title>
+      <StyledLink href='/mypage/mypost'><u>전체보기</u></StyledLink>
+      {memberLatestPost.length === 0 ? (
+        <NoResult style={{textAlign: "center"}}>😱작성된 글이 없습니다. </NoResult>
+      ) : (
+        <MyPostContainer>
+        {memberLatestPost.map((memberPost) => (
+        <Link key={memberPost.postNum} to={`/post/${memberPost.postNum}`}>
+          <PostTitle className='ellipsis'>{memberPost.postTitle}</PostTitle>
+          {/* <PostContent>{memberPost.postContent}</PostContent> */}
+          <ExtraInfo>
+            <i>{memberPost.boardName} {memberPost.writeDate}</i>
+          </ExtraInfo>
+        </Link>
+        ))}
+      </MyPostContainer>
+    )}
+  </ParentContainer>
+  );
+};
+
+export default MemberLatestPost;
---
+++ src/components/Account/MemberLatestReply.js
@@ -0,0 +1,112 @@
+import { useState, useEffect } from 'react';
+import AccountAxiosApi from '../../api/AccountAxiosApi';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const ParentContainer = styled.div`
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 20px;
+  box-shadow: 1px 1px 3px 1px #C6DEF7;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
+
+const Title = styled.h3`
+  align-self: flex-start;
+  margin-bottom: 10px;
+`;
+
+const StyledLink = styled.a`
+  position: absolute;
+  font-size: 0.9rem;
+  color: #1E2B4D;
+  align-self: flex-end;
+  text-decoration: underline;
+  margin-right: 10px;
+  &:hover {
+    font-weight: bold;
+  }
+`;
+
+const MyReplyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  & > *:not(:first-child) {
+    border-top: 1px solid #E5E7EA;
+  }
+  & > *:last-child {
+    border-bottom: 1px solid #E5E7EA;
+  }
+`;
+
+const ReplyContent = styled.p`
+  font-weight: bold;
+  font-size: 0.9rem;
+  margin-top: 10px;
+  width: 60%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis; 
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ExtraInfo = styled.p`
+  text-align: end;
+  font-size: 0.8rem;
+`;
+
+const NoResult = styled.p`
+  padding: 20%;
+  font-size: 1.2rem;
+`;
+
+const PostTitle = styled.p`
+  font-size: 0.9rem;
+`;
+
+const MemberLatestReply = ({ userMemberNum }) => {
+  const [memberLatestReply, setMemberLatestReply] = useState([]);
+
+  useEffect(() => {
+    const fetchMemberLatestReply = async () => {
+      try {
+        const response = await AccountAxiosApi.getMemberLatestReply(userMemberNum);
+        setMemberLatestReply(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMemberLatestReply();
+  }, [userMemberNum]);
+  
+  return (
+    <ParentContainer>
+    <Title>내가 작성한 댓글</Title>
+    <StyledLink href='/mypage/myreply'><u>전체보기</u></StyledLink>
+    {memberLatestReply.length === 0 ? (
+      <NoResult style={{textAlign: "center"}}>😱작성된 댓글이 없습니다. </NoResult>
+    ) : (
+      <MyReplyContainer>
+        {memberLatestReply.map((memberReply) => (
+          <Link key={memberReply.postNum} to={`/post/${memberReply.postNum}`}>
+            <ReplyContent className='ellipsis'>{memberReply.replyContent}</ReplyContent>
+            <ExtraInfo>
+              <i>{memberReply.boardName} {memberReply.writeDate}</i>
+            </ExtraInfo>
+          </Link>
+        ))}
+      </MyReplyContainer>
+    )}
+  </ParentContainer>
+);
+        };
+
+export default MemberLatestReply;
