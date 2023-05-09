c0de047693b06930edb6fdf84268135a7c24253b
dekim0705 <dekim0705@gmail.com>
2023-05-09 09:03:44 +0900

    :construction: 회원정보 수정 UI 1차 완료
    UI추가수정 필요

--- src/components/Account/MemberEditInformation.js
+++ src/components/Account/MemberEditInformation.js
@@ -2,7 +2,23 @@ import { useState, useEffect } from 'react';
 import AccountAxiosApi from '../../api/AccountAxiosApi';
 import { TextField, Select, MenuItem, InputLabel, FormControl } from '@mui/material';
 import styled from "styled-components";
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
+export const ParentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border-radius: 20px;
+  box-shadow: 1px 1px 3px 1px #C6DEF7;
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+`;
 
 const ResultField = styled.div`
   display: flex;
@@ -46,6 +62,7 @@ const StackName = styled.p`
 
 
 const MemberEditInformation = ({ userMemberNum }) => {
+  const navigate = useNavigate();
   const [currentMemberInfo, setCurrentMemberInfo] = useState([]);
   const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
@@ -63,37 +80,36 @@ const MemberEditInformation = ({ userMemberNum }) => {
     const fetchMemberCurrentInfo = async () => {
       try {
         const response = await AccountAxiosApi.getMemberCurrentInfo(userMemberNum);
-        console.log(response);
-        console.log(response.data);
-
-        setNickname(response.nickname);
-        setEmail(response.email);
-        setPassword(response.pwd);
-        setJob(response.job);
-        setYear(response.year);
-
+        console.log('현재 회원 정보 : ', response);
   
-        const updatedTechStacks = response.techStacks.map((stack) => ({
-          ...stack,
-          memberNum: userMemberNum,
-        }));
-        const updatedMemberInfo = {
-          ...response,
-          techStacks: updatedTechStacks,
-        };
-        setCurrentMemberInfo([updatedMemberInfo]); // 배열로 설정
-
-         // Set the member's current tech stack selection
- const selectedStacks = response.data.techStacks.map((stack) => stack.stackNum);
- setSelectedStacks(selectedStacks);
+        if (response) {
+          setNickname(response.nickname);
+          setEmail(response.email);
+          setPassword(response.pwd);
+          setJob(response.job);
+          setYear(response.year);
+  
+          const updatedTechStacks = response.techStacks.map((stack) => ({
+            ...stack,
+            memberNum: userMemberNum,
+          }));
+          const updatedMemberInfo = {
+            ...response,
+            techStacks: updatedTechStacks,
+          };
+          setCurrentMemberInfo([updatedMemberInfo]); // 배열로 설정
+  
+          // 회원이 현재 선택한 기술스택을 반영
+          const selectedStacks = response.techStacks.map((stack) => stack.stackNum);
+          setSelectedStacks(selectedStacks);
+        }
       } catch (error) {
         console.error(error);
       }
     };
   
     fetchMemberCurrentInfo();
-  }, [userMemberNum]);
-
+  }, [userMemberNum, setSelectedStacks]);
   
   
 
@@ -132,8 +148,6 @@ const MemberEditInformation = ({ userMemberNum }) => {
       try{
         const response = await AccountAxiosApi.allTechStacks();
         setTechStacks(response.data);
-        setSelectedStacks(selectedStacks);
-        // 
         console.log(response.data);
       } catch (error) {
         console.error("전체 기술스택 불러오기 에러 😰", error);
@@ -156,177 +170,208 @@ const MemberEditInformation = ({ userMemberNum }) => {
     setSearchTerm(e.target.value);
   };
 
-  const handleTechStackClick = (stackNum) => {
-    setSelectedStacks(prevSelectedStacks => {
-      if (prevSelectedStacks.includes(stackNum)) {
-        console.log('❌중복');
-        return prevSelectedStacks.filter(num => num !== stackNum);
-      } else {
-        console.log(prevSelectedStacks);
-        setSearchTerm("");
-        return [...prevSelectedStacks, stackNum];
+  const handleTechStackClick = async (stackNum) => {
+    if (selectedStacks.includes(stackNum)) {
+      // 이미 선택된 기술스택을 클릭한 경우
+      try {
+        console.log('삭제될 스택번호 :', stackNum)
+        await AccountAxiosApi.deleteStack(userMemberNum, stackNum);
+        setSelectedStacks((prevSelectedStacks) => prevSelectedStacks.filter(num => num !== stackNum));
+        console.log('✔️ 기술스택 삭제 성공');
+      } catch (error) {
+        console.log('❌ 기술스택 삭제 실패:', error);
       }
-    });
+    } else {
+      // 새로운 기술스택을 선택한 경우
+      try {
+        console.log('추가될 스택번호 :', stackNum)
+
+        await AccountAxiosApi.addStack(userMemberNum, stackNum);
+        setSelectedStacks((prevSelectedStacks) => [...prevSelectedStacks, stackNum]);
+        console.log('✔️ 기술스택 추가 성공');
+      } catch (error) {
+        console.log('❌ 기술스택 추가 실패:', error);
+      }
+    }
+  };
+
+
+
+  const updateMemberInfo = async () => {
+    try {
+      const memberInfo = {
+        memberNickname: nickname,
+        memberPwd: password,
+        memberJob: job,
+        memberYear: year,
+        memberTechStacks: selectedStacks,
+      };
+
+      const response = await AccountAxiosApi.updateMemberInfo(userMemberNum, memberInfo);
+      console.log("회원정보 수정 성공: ", response);
+
+    // 메인으로 갔다가 마이페이지로 이동
+    navigate("/");
+    setTimeout(() => {
+      navigate("/mypage");
+    }, 10); 
+
+    } catch (error) {
+      console.log("회원정보 수정 실패: ", error);
+    }
   };
 
 
 
   return (
-    <>
+    <ParentContainer>
+      
       {currentMemberInfo.map((currentInfo) => (
         <div key={currentInfo.memberNum}>
-          {/* <img src={currentInfo.pfImg} alt="profile" /> */}
+          <img src={currentInfo.pfImg} alt="profile" style={{width:200, height:200, borderRadius:100}}/>
+          <Button>프로필 사진 변경</Button>  
+            </div>))}
+      
 
-          <TextField 
-              size="small"
-              label="닉네임" 
-              value={nickname} 
-              onChange={handleNicknameChange} 
-              placeholder="닉네임을 입력하세요" 
-              required 
-              InputProps={{ sx: { borderRadius: 4 } }} 
-            /> 
+      <TextField 
+        size="small" 
+        label="닉네임" 
+        value={nickname} 
+        onChange={handleNicknameChange} 
+        placeholder="닉네임을 입력하세요" 
+        required 
+        InputProps={{ sx: { borderRadius: 4 } }} 
+      /> 
 
-            <TextField 
-              size="small" 
-              type="password"
-              label="비밀번호"
-              value={password}
-              onChange={handlePwdChange}
-              placeholder="비밀번호를 입력하세요"
-              required 
-              InputProps={{ sx: { borderRadius: 4 } }} 
-            />
-            <TextField 
-              size="small"
-              type="password" 
-              label="비밀번호 확인" 
-              value={conPassword} 
-              onChange={handleConPwdChange} 
-              placeholder="비밀번호를 다시 입력하세요" 
-              required 
-              InputProps={{ sx: { borderRadius: 4 } }} 
-            />
+      <TextField 
+        size="small" 
+        type="password"
+        label="비밀번호"
+        value={password}
+        onChange={handlePwdChange}
+        placeholder="비밀번호를 입력하세요"
+        required 
+        InputProps={{ sx: { borderRadius: 4 } }} 
+      />
 
-<TextField 
-            size="small" 
-            label="이메일주소" 
-            value={email} 
-            onChange={handleEmailChange} 
-            placeholder="이메일주소를 입력하세요" 
-            required 
-            InputProps={{ sx: { borderRadius: 4 } }} 
-          />
+      <TextField 
+        size="small"
+        type="password" 
+        label="비밀번호 확인" 
+        value={conPassword} 
+        onChange={handleConPwdChange} 
+        placeholder="비밀번호를 다시 입력하세요" 
+        required 
+        InputProps={{ sx: { borderRadius: 4 } }} 
+      />
+
+      <TextField 
+        size="small" 
+        label="이메일주소" 
+        value={email} 
+        onChange={handleEmailChange} 
+        placeholder="이메일주소를 입력하세요" 
+        required 
+        InputProps={{ sx: { borderRadius: 4 } }} 
+      />
 
 
-<FormControl sx={{ minWidth: 80 }} size="small">
-            <InputLabel>직업</InputLabel>                  
-            <Select
-              value={job}
-              label="직업"
-              onChange={onChangeJob}
-              autoWidth
-              sx={{ borderRadius: 4 }}
-              required
+      <FormControl sx={{ minWidth: 80 }} size="small">
+        <InputLabel>직업</InputLabel>                  
+        <Select
+          value={job}
+          label="직업"
+          onChange={onChangeJob}
+          autoWidth
+          sx={{ borderRadius: 4 }}
+          required
+        >
+          <MenuItem sx={{ borderRadius: 4 }} value="풀스택">풀스택</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value="백엔드">백엔드</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value="프론트엔드">프론트엔드</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value="학생">학생</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value="구직자">구직자</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl sx={{ minWidth: 80 }} size="small">
+        <InputLabel>연차</InputLabel>
+        <Select
+          value={year || ""}
+          label="연차"
+          onChange={onChangeCareerYear}
+          disabled={yearDisabled}
+          autoWidth
+          sx={{ borderRadius: 4 }}
+        >
+          <MenuItem sx={{ borderRadius: 4 }} value={1}>1년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={2}>2년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={3}>3년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={4}>4년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={5}>5년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={6}>6년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={7}>7년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={8}>8년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={9}>9년차</MenuItem>
+          <MenuItem sx={{ borderRadius: 4 }} value={10}>10년 이상</MenuItem>
+        </Select>
+      </FormControl>
+
+      <TextField 
+        size="small"
+        label="기술스택 검색" 
+        value={searchTerm} 
+        onChange={handleChange} 
+        placeholder="예) oracle" 
+        required 
+        InputProps={{ sx: { borderRadius: 4 } }} 
+      /> 
+      <ResultField>
+        {searchTerm ?
+          searchResults.map((techStack) => (
+            <SingleTechStack 
+              key={techStack.stackNum}
+              selected={selectedStacks.includes(techStack.stackNum)}
+              onClick={() => handleTechStackClick(techStack.stackNum)}
             >
-              <MenuItem sx={{ borderRadius: 4 }} value="풀스택">풀스택</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value="백엔드">백엔드</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value="프론트엔드">프론트엔드</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value="학생">학생</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value="구직자">구직자</MenuItem>
-            </Select>
-          </FormControl>
-
-          <FormControl sx={{ minWidth: 80 }} size="small">
-            <InputLabel>연차</InputLabel>
-            <Select
-              value={year || ""}
-              label="연차"
-              onChange={onChangeCareerYear}
-              disabled={yearDisabled}
-              autoWidth
-              sx={{ borderRadius: 4 }}
+              <img src={techStack.stackIconUrl} alt={techStack.stackName}
+                style={{
+                  width: 30,
+                  height: 30,
+                  borderRadius: "40%",  
+                }}
+              />
+              <StackName>{techStack.stackName}</StackName>
+            </SingleTechStack>
+          ))
+          :
+          techStacks.slice(0,6)
+          // .concat(techStacks.filter((techStack) => selectedStacks.includes(techStack.stackNum)))
+          .map((techStack) => (
+            <SingleTechStack
+              key={techStack.stackNum}
+              selected={selectedStacks.includes(techStack.stackNum)}
+              onClick={() => handleTechStackClick(techStack.stackNum)}
             >
-              <MenuItem sx={{ borderRadius: 4 }} value={1}>1년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={2}>2년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={3}>3년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={4}>4년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={5}>5년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={6}>6년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={7}>7년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={8}>8년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={9}>9년차</MenuItem>
-              <MenuItem sx={{ borderRadius: 4 }} value={10}>10년 이상</MenuItem>
-            </Select>
-          </FormControl>
-
-
-          <TextField 
-                  size="small"
-                  label="기술스택" 
-                  value={searchTerm} 
-                  onChange={handleChange} 
-                  placeholder="예) oracle" 
-                  required 
-                  InputProps={{ sx: { borderRadius: 4 } }} 
-                /> 
-            <ResultField>
-            {searchTerm ?
-              searchResults.map((techStack) => (
-                <SingleTechStack 
-                  key={techStack.stackNum}
-                  selected={selectedStacks.includes(techStack.stackNum)}
-                  onClick={() => handleTechStackClick(techStack.stackNum)}
-                >
-                  <img src={techStack.stackIconUrl} alt={techStack.stackName}
+                <img src={techStack.stackIconUrl} alt={techStack.stackName}
                     style={{
-                      width: 30,
-                      height: 30,
-                      borderRadius: "40%",  
+                        width: 30,
+                        height: 30,
+                        borderRadius: "40%",  
                     }}
-                  />
-                  <StackName>{techStack.stackName}</StackName>
-                </SingleTechStack>
-              ))
-              :
-              techStacks.slice(0,5)
-              // .concat(techStacks.filter((techStack) => selectedStacks.includes(techStack.stackNum)))
-              .map((techStack) => (
-                <SingleTechStack
-                  key={techStack.stackNum}
-                  selected={selectedStacks.includes(techStack.stackNum)}
-                  onClick={() => handleTechStackClick(techStack.stackNum)}
-                >
-                      <img src={techStack.stackIconUrl} alt={techStack.stackName}
-                          style={{
-                              width: 30,
-                              height: 30,
-                              borderRadius: "40%",  
-                          }}
-                      />
-                      <StackName>{techStack.stackName}</StackName>
-                  </SingleTechStack>
-                  
-              ))
-            }
-            </ResultField>
+                />
+                <StackName>{techStack.stackName}</StackName>
+              </SingleTechStack> 
+          ))
+        }
+        </ResultField>
 
-          {/* {currentInfo.nickname} */}
-          {/* <img src={currentInfo.gradeIconurl} alt="badge" /> */}
-          {/* {currentInfo.nickname}
-          {currentInfo.regDate}
-          {currentInfo.email}
-          {currentInfo.job}
-          {currentInfo.year} */}
-          {/* {currentInfo.techStacks.map((techStack) => ( */}
-            {/* // <div key={techStack.stackNum}> */}
-              {/* 기술 스택의 아이콘 정보 표시 */}
-             {/* <img src={techStack.stackIconUrl} alt="tech-stack-icon" /> */}
-            {/* </div> */}
-          {/* ))} */}
-        </div>
-      ))}
-    </>
+<div>
+          <Button onClick={updateMemberInfo}> 내 정보 수정 </Button>
+          <Button onClick={updateMemberInfo}>  회원 탈퇴 </Button>
+</div>
+
+    </ParentContainer>
   );
 };
 export default MemberEditInformation;
